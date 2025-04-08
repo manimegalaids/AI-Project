f49dc53eb40ede7b399d0ecc42eb9315096bbb16
Manimegalai T <158820822+manimegalaids@users.noreply.github.com>
2025-04-09 00:13:15 +0530

    Delete model.py

--- model.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-
-model_name = "bert-base-uncased"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
-
-# Save locally
-tokenizer.save_pretrained("./bert-base-uncased")
-model.save_pretrained("./bert-base-uncased")
